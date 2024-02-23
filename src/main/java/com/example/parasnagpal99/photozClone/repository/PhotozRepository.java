9621a06404b50c2d17aae8ced7448d8b391379e0
PARASnagpal99 <paras.nagpal743@gmail.com>
2024-02-24 03:33:59 +0530

    first commit

---
+++ src/main/java/com/example/parasnagpal99/photozClone/repository/PhotozRepository.java
@@ -0,0 +1,9 @@
+package com.example.parasnagpal99.photozClone.repository;
+
+import com.example.parasnagpal99.photozClone.model.Photo;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface PhotozRepository extends CrudRepository<Photo, Integer> {
+
+}
---
+++ src/main/java/com/example/parasnagpal99/photozClone/service/PhotozService.java
@@ -0,0 +1,41 @@
+package com.example.parasnagpal99.photozClone.service;
+
+import com.example.parasnagpal99.photozClone.model.Photo;
+import com.example.parasnagpal99.photozClone.repository.PhotozRepository;
+import org.springframework.stereotype.Service;
+
+
+import java.util.UUID;
+
+
+//@Component
+@Service
+public class PhotozService {
+
+    private final PhotozRepository photozRepository;
+
+    public PhotozService(PhotozRepository photozRepository) {
+        this.photozRepository = photozRepository;
+    }
+
+    public Iterable<Photo> get() {
+        return photozRepository.findAll();
+    }
+
+    public Photo get(Integer id) {
+        return photozRepository.findById(id).orElse(null);
+    }
+
+    public void remove(Integer id) {
+        photozRepository.deleteById(id);
+    }
+
+    public Photo save(String fileName, String contentType, byte[] data) {
+        Photo photo = new Photo();
+        photo.setContentType(contentType);
+        photo.setFileName(fileName);
+        photo.setData(data);
+        photozRepository.save(photo);
+        return photo;
+    }
+}
